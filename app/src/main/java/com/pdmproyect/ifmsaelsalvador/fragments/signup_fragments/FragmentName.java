04f21f197600152d85656a4b58996792262a38c4
petrlr14 <00057616@uca.edu.sv>
2018-05-19 22:21:34 -0600

    name fragment modifed

--- app/src/main/java/com/pdmproyect/ifmsaelsalvador/fragments/signup_fragments/FragmentName.java
+++ app/src/main/java/com/pdmproyect/ifmsaelsalvador/fragments/signup_fragments/FragmentName.java
@@ -1,9 +1,9 @@
 package com.pdmproyect.ifmsaelsalvador.fragments.signup_fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +15,15 @@ import com.stepstone.stepper.VerificationError;
 
 public class FragmentName extends android.support.v4.app.Fragment implements Step {
 
-    EditText editText;
+    TextInputEditText fname, lname, email;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.sign_up_name_layout, container, false);
-        editText=view.findViewById(R.id.edit_text_name);
+        fname=view.findViewById(R.id.edit_text_first_name);
+        lname=view.findViewById(R.id.edit_text_last_name);
+        email=view.findViewById(R.id.edit_text_email);
         return view;
     }
 
