0e986d6f1376eafa31935820524b19a49bf0cd62
petrlr14 <00057616@uca.edu.sv>
2018-05-20 09:08:38 -0600

    activities moved to activities package

--- app/src/main/java/com/pdmproyect/ifmsaelsalvador/activities/ProfileActivity.java
+++ app/src/main/java/com/pdmproyect/ifmsaelsalvador/activities/ProfileActivity.java
@@ -1,20 +1,21 @@
-package com.pdmproyect.ifmsaelsalvador;
+package com.pdmproyect.ifmsaelsalvador.activities;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.pdmproyect.ifmsaelsalvador.fragments.signup_fragments.SignUpCollegeActivity;
+import com.pdmproyect.ifmsaelsalvador.R;
 
 public class ProfileActivity extends AppCompatActivity {
-
-    SignUpCollegeActivity fragmento = new SignUpCollegeActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-      // getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragmento).commit();
+        /*
+        * para probra signup
+        *startActivity(new Intent(this, SignUpActivity.class));
+        * */
     }
 
 }
