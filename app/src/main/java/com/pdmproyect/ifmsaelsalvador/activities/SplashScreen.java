f4bea7d8791052783bba1faf4a1200e66aa155b7
ImperfectID <00172216@uca.edu.sv>
2018-07-05 17:42:12 -0600

    SplashScreen

---
+++ app/src/main/java/com/pdmproyect/ifmsaelsalvador/activities/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.pdmproyect.ifmsaelsalvador.activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.pdmproyect.ifmsaelsalvador.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int DURACION_SPLASH = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+                finish();
+            };
+
+        },DURACION_SPLASH);
+
+    }
+}
