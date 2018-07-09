59d91fa057f013118b43cd1a48133d092731d51f
Meight10 <00267016@uca.edu.sv>
2018-07-08 22:31:45 -0600

    creando metodo para para hacer el api service de subir un proyecto

--- app/src/main/java/com/pdmproyect/ifmsaelsalvador/api/ClientRequest.java
+++ app/src/main/java/com/pdmproyect/ifmsaelsalvador/api/ClientRequest.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.pdmproyect.ifmsaelsalvador.activities.CompleteFormActivity;
+import com.pdmproyect.ifmsaelsalvador.api.deserializers.PostProjectsDeserializer;
 import com.pdmproyect.ifmsaelsalvador.api.deserializers.ProjectDeserializer;
 import com.pdmproyect.ifmsaelsalvador.database.IFMSAViewModel;
 import com.pdmproyect.ifmsaelsalvador.database.entities.ProjectEntity;
@@ -49,7 +51,7 @@ public class ClientRequest {
                         new NewUser(
                                 user.getUsername(),
                                 user.getName(),
-                                user.getYear().charAt(0)+"",
+                                user.getYear().charAt(0) + "",
                                 user.getCollege(),
                                 user.getPassword(),
                                 user.getEmail(),
@@ -62,7 +64,7 @@ public class ClientRequest {
                 if (response.isSuccessful()) {
                     if (response.code() == 201) {
                         context.startActivity(new Intent(context, CompleteFormActivity.class));
-                        ((Activity)context).finish();
+                        ((Activity) context).finish();
                     }
                 }
                 System.out.println(response.code());
@@ -79,20 +81,20 @@ public class ClientRequest {
         return result;
     }
 
-    public static void login(Login login, Context context){
+    public static void login(Login login, Context context) {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(String.class, new TokenDeserializer())
                 .create();
-        Call<String> call=getClient(gson)
+        Call<String> call = getClient(gson)
                 .login(login);
         call.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                String token=response.body();
-                if (response.code()==200) {
+                String token = response.body();
+                if (response.code() == 200) {
                     saveToken(context, token);
                     context.startActivity(new Intent(context, MainActivity.class));
-                    ((Activity)context).finish();
+                    ((Activity) context).finish();
                 }
             }
 
@@ -103,19 +105,49 @@ public class ClientRequest {
         });
     }
 
-    public static void getProjects(IFMSAViewModel viewModel, String token){
+    public static void createProject(Project project, String token) {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(String.class, new PostProjectsDeserializer())
+                .create();
+        Call<String> call = getClient(gson).
+                postProjects("Bearer " + token, new Project(
+                        project.getCommittee(),
+                        project.getName(),
+                        project.getPlace(),
+                        project.getDescription(),
+                        project.getDate(),
+                        project.getVacancies()
+                ));
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    Log.i("CREATE PROJECT: ", "SUCCEFULL ");
+                } else {
+                    Log.i("CREATE PROJECT: ", "UNSUCCEFULL ");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.i("CREATE PROJECT: ", "FAILURE");
+            }
+        });
+    }
+
+    public static void getProjects(IFMSAViewModel viewModel, String token) {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Project.class, new ProjectDeserializer())
                 .create();
         System.out.println(token);
-        Call<List<Project>> getProjects=getClient(gson).getProjects("Bearer "+token);
+        Call<List<Project>> getProjects = getClient(gson).getProjects("Bearer " + token);
         getProjects.enqueue(new Callback<List<Project>>() {
             @Override
             public void onResponse(Call<List<Project>> call, Response<List<Project>> response) {
                 System.out.println(response.code());
-                if(response.code()==200){
+                if (response.code() == 200) {
                     for (Project project : response.body()) {
-                        ProjectEntity entity=
+                        ProjectEntity entity =
                                 new ProjectEntity(
                                         project.getId(),
                                         project.getCommittee(),
@@ -138,10 +170,10 @@ public class ClientRequest {
 
     }
 
-    private static void saveToken(Context context, String token){
+    private static void saveToken(Context context, String token) {
         System.out.println(token);
-        SharedPreferences preferences=context.getSharedPreferences("log", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor=preferences.edit();
+        SharedPreferences preferences = context.getSharedPreferences("log", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
         editor.putString("token", token);
         editor.commit();
     }
